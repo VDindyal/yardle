8dc0781400adf4e8a0c1b8d9b6b5b8f688cf6e1f
VDindyal <43999426+VDindyal@users.noreply.github.com>
2020-04-23 17:02:11 -0400

    Added multiple pages and re-did home page

--- yardlecore/listing/urls.py
+++ yardlecore/listing/urls.py
@@ -5,5 +5,9 @@ from . import views
 app_name = 'listing'
 urlpatterns = [
     path('create', views.create, name='create'),
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('construction', views.construction, name='construction'),
+    path('financing', views.financing, name='financing'),
+    path('getstarted', views.getstarted, name='getstarted'),
+    path('property', views.property, name='property'),
 ]
--- yardlecore/listing/views.py
+++ yardlecore/listing/views.py
@@ -12,3 +12,23 @@ def create(request):
     template = loader.get_template("listing/create.html")
     context = {}
     return HttpResponse(template.render(context, request))
+
+def construction(request):
+    template = loader.get_template("listing/construction.html")
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def financing(request):
+    template = loader.get_template("listing/financing.html")
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def getstarted(request):
+    template = loader.get_template("listing/getstarted.html")
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def property(request):
+    template = loader.get_template("listing/property.html")
+    context = {}
+    return HttpResponse(template.render(context, request))
