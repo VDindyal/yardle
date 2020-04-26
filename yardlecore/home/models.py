bc8ff7a9793fc36bbda47b4196e92b78695ae030
Michael <michael_kirkpatrick@outlook.com>
2020-04-26 19:03:40 -0400

    User Groups, Sessions, Sign up, Sign in, and access permissions

--- yardlecore/home/models.py
+++ yardlecore/home/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    corp_name = models.CharField(max_length=128, null=True, blank=False)
+    years_incorporated = models.IntegerField(null=True, blank=False)
+    service_cost = models.FloatField(null=True, blank=False)
+
+    CORP_TYPES = (
+        ('Construction', 'Construction'),
+        ('Financing', 'Financing'),
+        ('Property Management', 'Property Management'))
+    corp_type = models.CharField(max_length=64, choices=CORP_TYPES, null=True, blank=False)
+
+@receiver(post_save, sender=User)
+def update_profile_signal(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
--- yardlecore/home/urls.py
+++ yardlecore/home/urls.py
@@ -6,6 +6,8 @@ app_name = 'home'
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup', views.signup, name='signup'),
+    path('signup_corporate', views.signup_corporate, name='signup_corporate'),
+    path('signin', views.signin, name='signin'),
     path('how', views.how, name="how"),
     path('logout', views.user_logout, name='user_logout')
 ]
