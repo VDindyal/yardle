5b0f038efeeb4d07f1920b5e4dd4547aea63ec92
Michael <michael_kirkpatrick@outlook.com>
2020-04-27 20:41:41 -0400

    Prod deployment config

--- yardlecore/yardlecore/settings.py
+++ yardlecore/yardlecore/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -132,3 +133,5 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'home/home'
 
 LOGIN_URL = 'home:signin'
+
+django_heroku.settings(locals())
